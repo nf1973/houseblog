802f0fb039d595e229b187da266f91cda229595b
nf1973 <56644716+nf1973@users.noreply.github.com>
2024-01-13 09:30:37 +0100

    fix &s

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/google28e1934b71ec8acc.html",
-        destination: "google28e1934b71ec8acc.html",
+        destination: "/google28e1934b71ec8acc.html",
       },
     ];
   },
