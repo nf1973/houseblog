db650ea1ec35f8e38a2c5980df1733d8bc81fea2
nf1973 <56644716+nf1973@users.noreply.github.com>
2024-01-13 09:15:14 +0100

    add rewrite

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/google28e1934b71ec8acc.html",
+        destination: "google28e1934b71ec8acc.html",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
